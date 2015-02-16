b233e6d12749d3fa3021dd3686657ef61d5c6179
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-02-16 16:12:12 -0600

    remove junk file

--- .mp1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/cs423/MP1/mp1.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-44-generic/scripts/module-common.lds --build-id  -o /home/cs423/MP1/mp1.ko /home/cs423/MP1/mp1.o /home/cs423/MP1/mp1.mod.o
--- .tmp_versions/mp1.mod
+++
@@ -1,2 +0,0 @@
-/home/cs423/MP1/mp1.ko
-/home/cs423/MP1/mp1.o
